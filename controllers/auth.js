1594f8e223d43ff5c30728862df2ecea5af4c4bd
Piya-Boy <87295280+Piya-Boy@users.noreply.github.com>
2024-02-24 19:04:50 +0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -103,7 +103,8 @@ const login = async (req, res, next) => {
 
         res.cookie("accessToken", token, {
             secure: true,      
-            httpOnly: true
+            httpOnly: true,
+            sameSite: 'Strict'
         });
 
         return res.status(200).json(others);
